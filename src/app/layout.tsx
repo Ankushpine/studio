ab2ee4ab10a8abc8893dbb313c3247b5d773a2aa
Ankush Pine <smuthpine@gmail.com>
2025-09-20 08:06:14 +0000

    can you add some animated pictures in background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { AppProvider } from '@/components/app-provider';
 import { Toaster } from '@/components/ui/toaster';
 import { Inter } from 'next/font/google';
+import { AnimatedBackground } from '@/components/animated-background';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
@@ -22,7 +23,10 @@ export default function RootLayout({
       <head />
       <body className={`${inter.variable} font-body antialiased`}>
         <AppProvider>
-          {children}
+          <AnimatedBackground />
+          <div className="relative z-10">
+            {children}
+          </div>
           <Toaster />
         </AppProvider>
       </body>
